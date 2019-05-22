50f3bcc7529881ac3d8aa414d9f12604c2264d46
hridayns <hridaysanghvi@gmail.com>
2019-05-22 19:49:59 +0530

    update http to https for OWM API endpoint

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ $(function() {
   // $.getJSON("https://ip-api.com/json",function(data) {
   $.getJSON("https://cors.io/?http://ip-api.com/json",function(data) { //to get through mixed content block (HTTPS)
     console.log(data.lat + "," + data.lon);
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + data.lat + "&lon=" + data.lon + "&appid=" + OPEN_WEATHER_MAP_API_KEY,function(data) {
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + data.lat + "&lon=" + data.lon + "&appid=" + OPEN_WEATHER_MAP_API_KEY,function(data) {
       console.log("Weather: " + data.weather[0].main);
       console.log("Temp in C: " + (data.main.temp-273.15));
       console.log("Temp in F: " + (((data.main.temp-273.15) * 1.8) + 32));
