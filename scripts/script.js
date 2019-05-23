b9a9e7c456ffc8f2fe73bc9be7bd1ab0ec21ea47
hridayns <hridaysanghvi@gmail.com>
2019-05-23 09:17:27 +0530

    update HTTPS API endpoint for geolocation data

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 var OPEN_WEATHER_MAP_API_KEY = "d78b40d9b884b3b0ce4db0a347f1dfa2"; //replace with your own API key
 $(function() {
   // $.getJSON("https://ip-api.com/json",function(data) {
-  $.getJSON("https://cors.io/?http://ip-api.com/json",function(data) { //to get through mixed content block (HTTPS)
-    console.log(data.lat + "," + data.lon);
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + data.lat + "&lon=" + data.lon + "&appid=" + OPEN_WEATHER_MAP_API_KEY,function(data) {
+  // https://cors.io/?http://ip-api.com/json - to get through mixed content block (HTTPS)
+  $.getJSON("https://ipapi.co/json/",function(data) { 
+    lat = data.latitude;
+    lon = data.longitude;
+    console.log(lat + "," + lon);
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + OPEN_WEATHER_MAP_API_KEY,function(data) {
       console.log("Weather: " + data.weather[0].main);
       console.log("Temp in C: " + (data.main.temp-273.15));
       console.log("Temp in F: " + (((data.main.temp-273.15) * 1.8) + 32));
